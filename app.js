1ab1225b4575860416a7c5c80a63705d57375cbd
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-10 08:15:16 +0545

    remove dummy code

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 const path = require("path");
-// for parsing text
-const bodyParser = require("body-parser");
-
+const bodyParser = require("body-parser"); // for parsing text
 const express = require("express");
-
 const app = express();
 
 const adminRoutes = require("./routes/admin"); //
 const shopRoutes = require("./routes/shop");
+
 // middleware starts
-app.use("/", (req, res, next) => {
-  res.send("<h1>hello</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "error404.html"));
 });
-app.use("/fol", (req, res, next) => {
-  console.log("exercute");
-});
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, "public")));
-
-// app.use("/admin", adminRoutes);
-
-// app.use(shopRoutes);
-
-// app.use((req, res, next) => {
-//   res.status(404).sendFile(path.join(__dirname, "views", "error404.html"));
-// });
 // middleware ends
 
 app.listen(4000);
