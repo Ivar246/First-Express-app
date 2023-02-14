14d0eacab1d275919ef2fea69d8b8385f0a2f021
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-14 15:17:00 +0545

    cleaned code

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const path = require("path");
-const bodyParser = require("body-parser"); // for parsing text
-const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
-const admin = require("./routes/admin"); //
+const admin = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
 const hbs = require("express-handlebars");
 // app.engine(
 //   "hbs",
@@ -19,18 +16,13 @@ const hbs = require("express-handlebars");
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// middleware starts
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, "public"))); // serving static file
 
 app.use("/admin", admin.routes);
-
 app.use(shopRoutes);
-
 app.use((req, res, next) => {
   res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
-// middleware ends
 
 app.listen(4000);
