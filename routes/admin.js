55f77aba76108002740d9e679b03e53fe10122e7
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-18 22:07:55 +0545

    routes to different url added

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,8 @@ const productController = require("../controllers/products");
 
 // admin/add-product=> GET
 router.get("/add-product", productController.getAddProduct);
+router.get("/admin-product");
 
 // admin/add-product=>POST
 router.post("/add-product", productController.postAddProduct);
-
 exports.routes = router;
--- routes/shop.js
+++ routes/shop.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const productController = require("../controllers/products");
 
 router.get("/", productController.getProducts);
-
+router.get("/products");
+router.get("/cart");
+router.get("/checkout");
 module.exports = router;
