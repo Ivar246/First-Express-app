ea5a73a960d1a19c0250fc4b3f4228d4c237a4be
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-19 13:22:54 +0545

    add style to the buttons

--- models/product.js
+++ models/product.js
@@ -23,6 +23,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(fPath, JSON.stringify(products), (err) => {
--- routes/shop.js
+++ routes/shop.js
@@ -7,4 +7,6 @@ router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 router.get("/cart", shopController.getCart);
 router.get("/checkout", shopController.getCheckout);
+router.get("/orders", shopController.getOrders);
+
 module.exports = router;
