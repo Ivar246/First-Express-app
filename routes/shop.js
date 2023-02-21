7ef2eab067ff4b0e8016d14f24c51f2df4a83bd6
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-21 13:56:37 +0545

    middleware and routes for add to cart functionality

--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,7 @@ const shopController = require("../controllers/shop");
 router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 router.get("/cart", shopController.getCart);
+router.post("/cart", shopController.postCart);
 router.get("/products/:productId", shopController.getProduct);
 // router.get('/products/delete')
 router.get("/checkout", shopController.getCheckout);
