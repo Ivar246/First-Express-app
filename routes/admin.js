f7911f1f2643a1f0062f983aa992e197610f4077
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-22 14:10:24 +0545

    route for delete-prdouct added

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,6 @@ router.get("/add-product", adminController.getAddProduct);
 router.get("/products", adminController.getProducts);
 router.get("/edit-product/:productId", adminController.getEditProduct);
 router.post("/edit-product", adminController.postEditProduct);
-// admin/add-product=>POST
+router.post("/delete-product", adminController.postDeleteProduct);
 router.post("/add-product", adminController.postAddProduct);
 exports.routes = router;
