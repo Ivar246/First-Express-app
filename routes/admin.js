1e69de4c82a315e7d2cf604826aabffa0d016a68
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-22 13:50:01 +0545

    route for postEdit added

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,7 @@ const adminController = require("../controllers/admin");
 router.get("/add-product", adminController.getAddProduct);
 router.get("/products", adminController.getProducts);
 router.get("/edit-product/:productId", adminController.getEditProduct);
+router.post("/edit-product", adminController.postEditProduct);
 // admin/add-product=>POST
 router.post("/add-product", adminController.postAddProduct);
 exports.routes = router;
