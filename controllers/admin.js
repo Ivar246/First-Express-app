74b6212a0c9bc5a6229e9ff201ca598caf587b7c
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-22 09:09:07 +0545

    edit middleware added

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,19 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formCSS: true,
-    productCSS: true,
-    activeProduct: true,
+  });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) return res.redirect("/");
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/edit-product",
+    editing: editMode,
   });
 };
 
