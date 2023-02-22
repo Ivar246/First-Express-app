3d9e9040b620274e87d5bbc669271e398b11aeeb
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-22 09:09:43 +0545

    routes for edit

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const adminController = require("../controllers/admin");
 // admin/add-product=> GET
 router.get("/add-product", adminController.getAddProduct);
 router.get("/products", adminController.getProducts);
-
+router.get("/edit-product/:productId", adminController.getEditProduct);
 // admin/add-product=>POST
 router.post("/add-product", adminController.postAddProduct);
 exports.routes = router;
