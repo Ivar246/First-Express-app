3cb3026a6ddd43a42834d0d758f3b3b3f08e2d86
IVAR AHTSERHS <ravistha869@gmail.com>
2023-02-25 00:28:38 +0545

    route added: cart-delete-item

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ router.get("/products", shopController.getProducts);
 router.get("/cart", shopController.getCart);
 router.post("/cart", shopController.postCart);
 router.get("/products/:productId", shopController.getProduct);
-// router.get('/products/delete')
+router.post("/cart-delete-item", shopController.postDeleteCart);
 router.get("/checkout", shopController.getCheckout);
 router.get("/orders", shopController.getOrders);
 
