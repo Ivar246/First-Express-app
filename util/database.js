ec38dbf265183f30f635212b338d607a96ace4f9
IVAR AHTSERHS <ravistha869@gmail.com>
2023-03-03 14:37:29 +0545

    configure sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "Shop",
+//   password: "Root@123",
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("Shop", "root", "Root@123", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "shop",
-  password: "Root@123",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
