9c8f64ae37f29ce42dfc07cfc628e947f0b86aef
IVAR AHTSERHS <ravistha869@gmail.com>
2023-03-08 19:23:29 +0545

    special method of sequelize used

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-    userId: req.user.id,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+      userId: req.user.id,
+    })
+
     .then((result) => {
       res.redirect("/");
     })
