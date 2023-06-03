317e850374050b4c78f576e981a5a24edc9bc6fc
ivar246 <ravistha869@gmail.com>
2023-06-03 15:46:48 +0545

    route set with mongoose

--- routes/shop.js
+++ routes/shop.js
@@ -13,11 +13,11 @@ router.post("/cart", shopController.postCart);
 
 router.get("/products/:productId", shopController.getProduct);
 
-// router.post("/cart-delete-item", shopController.postDeleteCart);
+router.post("/cart-delete-item", shopController.postDeleteCart);
 
-// router.post("/create-order", shopController.postOrder);
+router.post("/create-order", shopController.postOrder);
 
-// router.get("/orders", shopController.getOrders);
+router.get("/orders", shopController.getOrders);
 
 // router.get("/checkout", shopController.getCheckout);
 
