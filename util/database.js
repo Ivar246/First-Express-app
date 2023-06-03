102cd1bb10d22957fe111792c18040b90032cdae
ivar246 <ravistha869@gmail.com>
2023-06-03 15:48:54 +0545

    ...

--- util/database.js
+++ util/database.js
@@ -9,12 +9,42 @@
 
 // module.exports = pool.promise();
 
-const Sequelize = require("sequelize");
+// const Sequelize = require("sequelize");
 
-// instance of mysql database
-const sequelize = new Sequelize("Shop", "root", "Root@123", {
-  dialect: "mysql",
-  host: "localhost",
-});
+// // instance of mysql database
+// const sequelize = new Sequelize("Shop", "root", "Root@123", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db; 
+
+const MongoConnect = (callback)=>{
+MongoClient.connect("mongodb+srv://ravistha:root123@cluster0.x0jnnah.mongodb.net/?retryWrites=true&w=majority")
+.then(client=>{
+  console.log("connected");
+  _db = client.db();
+  
+  callback();
+})
+.catch(err=>{
+  console.log("Error while connecting mongodb");
+  throw err
+})
+}
+
+
+const getDb = () =>{
+  if(_db){
+    return _db;
+  }
+  throw "No database found"
+}
+
+exports.mongoConnect  = MongoConnect;
+exports.getDb = getDb;
