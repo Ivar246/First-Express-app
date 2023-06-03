d34fa1f9d187640db9bb43a52d3c0c03d452fd86
ivar246 <ravistha869@gmail.com>
2023-06-03 15:44:25 +0545

    model for order

--- models/orders.js
+++ models/orders.js
@@ -1,13 +1,39 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../util/database");
+const mongoose = require("mongoose");
+const { schema } = require("./product");
 
-const Order = sequelize.define("order", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+  products: [{
+    productData: { type: Object, required: true },
+    quantity: { type: Number, required: true }
+  }],
+  user: {
+    name: { type: String, required: true },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User"
+    }
+  }
 });
 
-module.exports = Order;
+
+module.exports = mongoose.model("Order", OrderSchema);
+
+
+
+
+// const Sequelize = require("sequelize");
+// const sequelize = require("../util/database");
+
+// const Order = sequelize.define("order", {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true,
+//   },
+// });
+
+// module.exports = Order;
