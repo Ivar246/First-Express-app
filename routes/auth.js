66662d955e96ffa6b25f018a34b290c000400609
ivar246 <ravistha869@gmail.com>
2023-06-04 18:38:14 +0545

    auth route added

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const authContorllers = require("../controllers/auth")
 
-router.get('/login', (req, res, next) => {
 
-});
-
+router.get('/login', authContorllers.getLogin);
+router.post("/login", authContorllers.postLogin)
 module.exports = router
