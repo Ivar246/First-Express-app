4fdf53127b6369e462471d0c2749872066c6745f
ivar246 <ravistha869@gmail.com>
2023-06-04 18:36:26 +0545

    ..

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,16 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req.get("Cookie")
+        .split("=").pop() === "true";
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
+        isAuthenticated: isLoggedIn
     })
+
+}
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader("Set-Cookie", "loggedIn=true; httpOnly");
+    res.redirect("/")
 
 }
