5d85593d993439e3087d5b4505f3729029216558
ivar246 <ravistha869@gmail.com>
2023-06-05 21:05:32 +0545

    work with session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,29 @@
+const User = require("../models/user")
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req.get("Cookie")
-        .split("=").pop() === "true";
+    // const isLoggedIn = req.get("Cookie")
+    //     .split("=").pop() === "true";
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     })
 
 }
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader("Set-Cookie", "loggedIn=true; httpOnly");
-    res.redirect("/")
+    User.findById("6479db820b5953c576f2edd2")
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            res.redirect("/")
+        })
+        .catch(err => console.log(err))
+}
 
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        if (err) console.log(err);
+        res.redirect("/")
+
+    })
 }
