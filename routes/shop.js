dbbc253f8fc6672aa13f8161254811ec41ff3eda
ivar246 <ravistha869@gmail.com>
2023-06-06 22:39:47 +0545

    route protection with isAuth middleware

--- routes/shop.js
+++ routes/shop.js
@@ -2,22 +2,24 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is_auth");
+
 
 router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
-router.get("/cart", shopController.getCart);
+router.get("/cart", isAuth, shopController.getCart);
 
-router.post("/cart", shopController.postCart);
+router.post("/cart", isAuth, shopController.postCart);
 
-router.get("/products/:productId", shopController.getProduct);
+router.get("/products/:productId", isAuth, shopController.getProduct);
 
-router.post("/cart-delete-item", shopController.postDeleteCart);
+router.post("/cart-delete-item", isAuth, shopController.postDeleteCart);
 
-router.post("/create-order", shopController.postOrder);
+router.post("/create-order", isAuth, shopController.postOrder);
 
-router.get("/orders", shopController.getOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
 // router.get("/checkout", shopController.getCheckout);
 
