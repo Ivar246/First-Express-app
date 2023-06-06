3d764d474cb9918f16461e291684e686a3d72f3e
ivar246 <ravistha869@gmail.com>
2023-06-06 22:38:28 +0545

    isAuth added in every route

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,15 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin");
+const isAuth = require("../middleware/is_auth");
 
 // admin/add-product=> GET
-router.get("/add-product", adminController.getAddProduct);
-router.get("/products", adminController.getProducts);
-router.get("/edit-product/:productId", adminController.getEditProduct);
-router.post("/edit-product", adminController.postEditProduct);
-router.post("/delete-product", adminController.postDeleteProduct);
-router.post("/add-product", adminController.postAddProduct);
+router.get("/add-product", isAuth, adminController.getAddProduct);
+router.get("/products", isAuth, adminController.getProducts);
+router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
+router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post("/delete-product", isAuth, adminController.postDeleteProduct);
+router.post("/add-product", isAuth, adminController.postAddProduct);
 
 
 exports.routes = router;
