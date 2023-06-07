54afc11026fcc957ec5af227b2e6b84a9cb2face
ivar246 <ravistha869@gmail.com>
2023-06-07 23:04:24 +0545

    restriction added to login and signup route if already loggedIn

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const authContorllers = require("../controllers/auth")
+const { loggedIn } = require("../middleware/is_auth")
 
-
-router.get('/login', authContorllers.getLogin);
+router.get('/login', loggedIn, authContorllers.getLogin);
 
 router.post("/login", authContorllers.postLogin)
 
 router.post("/logout", authContorllers.postLogout)
 
-router.get("/signup", authContorllers.getSignup)
+router.get("/signup", loggedIn, authContorllers.getSignup)
 
 router.post("/signup", authContorllers.postSignup)
 module.exports = router
