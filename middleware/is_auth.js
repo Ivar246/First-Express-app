740d8ef3f660eb6f119c2231d44487774f3e0ce4
ivar246 <ravistha869@gmail.com>
2023-06-07 23:00:48 +0545

    loggedIn added

--- middleware/is_auth.js
+++ middleware/is_auth.js
@@ -1,6 +1,18 @@
-module.exports = (req, res, next) => {
+const isAuth = (req, res, next) => {
     if (!req.session.isLoggedIn) {
         return res.redirect("/login")
     }
     next();
+}
+
+const loggedIn = (req, res, next) => {
+    if (req.session.isLoggedIn) {
+        return res.redirect("/")
+    }
+    next();
+}
+
+module.exports = {
+    isAuth,
+    loggedIn
 }
