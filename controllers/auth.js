2bdc34daf11bc5a3d9a5560aaea9fe4a2affce0d
ivar246 <ravistha869@gmail.com>
2023-06-14 13:43:24 +0545

    validation added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const crypto = require("crypto");
 const bcrypt = require("bcryptjs")
+const { } = require("express-validator/check")
 
 const User = require("../models/user")
 const { isValidEmail } = require("../validator/validate")
@@ -130,7 +131,7 @@ exports.postReset = (req, res, next) => {
             console.log(err);
             return res.redirect("/reset")
         }
-        console.log("buffer", buffer)
+        console.log(buffer.toString("base64"))
         const token = buffer.toString('hex');
         User.findOne({ email: req.body.email })
             .then(user => {
