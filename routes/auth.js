72dbd7ab6b40c07f9cfd211a635499ef6369f8cf
ivar246 <ravistha869@gmail.com>
2023-06-14 13:44:21 +0545

    validation added

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const { check } = require("express-validator")
 const router = express.Router();
 const authContorllers = require("../controllers/auth")
 const { loggedIn } = require("../middleware/is_auth")
+
+
 
 router.get('/login', loggedIn, authContorllers.getLogin);
 
@@ -11,7 +14,7 @@ router.post("/logout", authContorllers.postLogout)
 
 router.get("/signup", loggedIn, authContorllers.getSignup)
 
-router.post("/signup", authContorllers.postSignup)
+router.post("/signup", check("email").isEmail(), authContorllers.postSignup)
 
 router.get("/reset", authContorllers.getReset)
 
