4d8f310249b54b7719dc039478d3571f86ea52e5
ivar246 <ravistha869@gmail.com>
2023-06-15 16:56:15 +0545

    validator added in post route

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,37 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin");
 const { isAuth } = require("../middleware/is_auth");
+const { body } = require("express-validator");
+
+
+
+//  /products => GET
+router.get("/products", isAuth, adminController.getProducts);
 
 // admin/add-product=> GET
 router.get("/add-product", isAuth, adminController.getAddProduct);
-router.get("/products", isAuth, adminController.getProducts);
+
+//  admin/add-product => POST
+router.post("/add-product", [
+    body("title").isString().trim().isLength({ min: 3 }).withMessage("title should at least 3 character long."),
+    body("imageUrl").isURL().withMessage("invalid image url."),
+    body("price").isNumeric(),
+    body("description").trim().isLength({ min: 5 })
+], isAuth, adminController.postAddProduct);
+
+//  /admin/edit-product => GET
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+
+//   /admin/edit-product =>POST
+router.post("/edit-product", [
+    body("title").isString().trim().isLength({ min: 3 }).withMessage("title should be at least 3 character long."),
+    body("imageUrl").isURL().withMessage('invalid image url.'),
+    body("price").isFloat(),
+    body("description").trim().isLength({ min: 5 })
+], isAuth, adminController.postEditProduct);
+
+//  admin/delete-product =>POST 
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
-router.post("/add-product", isAuth, adminController.postAddProduct);
 
 
 exports.routes = router;
