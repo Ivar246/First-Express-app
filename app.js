a492f2422d534a0a7f3c051d12da6a4d2e310bb0
ivar246 <ravistha869@gmail.com>
2023-06-17 15:56:26 +0545

    error handling middleware added

--- app.js
+++ app.js
@@ -44,11 +44,16 @@ const User = require("./models/user")
 //   })
 // );
 
+connectDB();
+
 // template setting
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-
+app.use((req, res, next) => {
+    console.log(req.useragent);
+    next();
+})
 
 
 // parser
@@ -67,35 +72,51 @@ app.use(csrfProtection)
 app.use(flash())
 
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+})
+
+
+
+// serving static file
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// paths to sub paths
+app.use("/admin", admin.routes);
+app.use(shopRoutes);
+app.use(authRoutes);
+app.get("/500", errorController.get500)
+app.use(errorController.errorHandler);
+
+app.use((error, req, res, next) => {
+    res.status(500).render("500", {
+        pageTitle: "Error",
+        path: "/500",
+        isAuthenticated: req.session.isLoggedIn
+    });
+})
+
+app.use((req, res, next) => {
     if (!req.session.user) return next();
     User.findById(req.session.user._id)
         .then(user => {
+            if (!user) {
+                return next();
+            }
             req.user = user;
             next();
         })
-        .catch(err => console.log(err))
+        .catch(err => {
+            next(new Error(err))
+        })
 
 });
 
 
 
 // mongoose connection
-connectDB();
-
-// serving static file
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
-})
-
-// paths to sub paths
-app.use("/admin", admin.routes);
-app.use(shopRoutes);
-app.use(authRoutes)
-app.use(errorController.errorHandler);
 
 // Associations between models(relation between tables)
 // Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
