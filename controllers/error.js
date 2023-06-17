08df76df4981c13b0a81fc40b310607e62446456
ivar246 <ravistha869@gmail.com>
2023-06-17 15:48:16 +0545

    500 error middleware added

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,12 @@
 exports.errorHandler = (req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: null });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404", isAuthenticated: req.session.isLoggedIn });
 };
+
+exports.get500 = (req, res, next) => {
+  res.status(500).render("500", {
+    pageTitle: "Error",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn
+  });
+};
+
