3aace661ca5ce7c0843f3a47cd08aee589e494fd
ivar246 <ravistha869@gmail.com>
2023-06-19 21:10:33 +0545

    getInvoice rouce added

--- routes/shop.js
+++ routes/shop.js
@@ -21,6 +21,7 @@ router.post("/create-order", isAuth, shopController.postOrder);
 
 router.get("/orders", isAuth, shopController.getOrders);
 
+router.get("/orders/:orderId", isAuth, shopController.getInvoice)
 // router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
