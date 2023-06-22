26155b42393f0d7dbcd82ea8160a2d6bf18f75a2
ivar246 <ravistha869@gmail.com>
2023-06-22 23:48:03 +0545

    ..

--- app.js
+++ app.js
@@ -27,23 +27,6 @@ const store = new MongoDBStore({
 
 const csrfProtection = csrf()
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toString() + '-' + file.originalname)
-    }
-});
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
-        cb(null, true);
-    } else {
-        cb(null, false)
-    }
-}
-
 // module.exports = multer({ storage: storage, fileFilter: fileFilter }).single("image")
 
 
