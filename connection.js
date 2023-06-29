73928d61e6863e8b64f442d8da7499880062fc93
ivar246 <ravistha869@gmail.com>
2023-06-29 07:55:47 +0545

    ...

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
 
     try {
         // mongodb connection string
-        const con = await mongoose.connect("mongodb+srv://ravistha:root123@cluster0.x0jnnah.mongodb.net/?retryWrites=true&w=majority", {
+        const con = await mongoose.connect(process.env.DB, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
 
